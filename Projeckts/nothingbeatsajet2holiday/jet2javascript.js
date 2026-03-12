a90c4bd6224df9a0aad058b0dc6d122dd2ad2568
N3ON-B4qON <qlau808@gmail.com>
2026-03-12 08:09:35 -1000

    lots of adjustments

--- Projeckts/nothingbeatsajet2holiday/jet2javascript.js
+++ Projeckts/nothingbeatsajet2holiday/jet2javascript.js
@@ -3,7 +3,6 @@ let access101 = "unlocked"
 let dissable101 = false
 let max101 = 0
 let full101 = []
-let over101 = []
 let kicked101 = []
 let teimo101 = []
 let tiemo101 = []
@@ -13,7 +12,6 @@ function nothing1() {
         max101 +=1
         let room1flo1 = document.getElementById('eccono101').value
         full101.push(room1flo1)
-        over101.push(room1flo1)
         document.getElementById('occupant101').innerHTML = "occupant(s): " + full101
         document.getElementById('eccono101').value = ""
         document.getElementById('overflow101').innerHTML = ""
@@ -30,9 +28,6 @@ function beats1() {
     if (compare101 == full101[0] || compare101 == full101[1] || compare101 == full101[2] || compare101 == full101[3]) {
         if (compare101 == full101[0]) {
             max101 -=1
-            let exit101 = full101[0] + Date()
-            tiemo101.push(exit101)
-            localStorage.setItem('checkout101',tiemo101)
             // Remove Guest Part
             kicked101 = full101.splice(0,1)
             document.getElementById('occupant101').innerHTML = "occupant(s): " + full101
@@ -41,9 +36,6 @@ function beats1() {
         }
         else if (compare101 == full101[1]) {
             max101 -=1
-            let exit101 = full101[1] + Date()
-            tiemo101.push(exit101)
-            localStorage.setItem('checkout101',tiemo101)
             // Remove Guest Part
             kicked101 = full101.splice(1,1)
             document.getElementById('occupant101').innerHTML = "occupant(s): " + full101
@@ -52,9 +44,6 @@ function beats1() {
         }
         else if (compare101 == full101[2]) {
             max101 -=1
-            let exit101 = full101[2] + Date()
-            tiemo101.push(exit101)
-            localStorage.setItem('checkout101',tiemo101)
             // Remove Guest Part
             kicked101 = full101.splice(2,1)
             document.getElementById('occupant101').innerHTML = "occupant(s): " + full101
@@ -63,9 +52,6 @@ function beats1() {
         }
         else {
             max101 -=1
-            let exit101 = full101[3] + Date()
-            tiemo101.push(exit101)
-            localStorage.setItem('checkout101',tiemo101)
             // Remove Guest Part
             kicked101 = full101.splice(3,1)
             document.getElementById('occupant101').innerHTML = "occupant(s): " + full101
@@ -112,7 +98,6 @@ let access102 = "unlocked"
 let dissable102 = false
 let max102= 0
 let full102 = []
-let over102 = []
 let kicked102 = []
 let teimo102 = []
 let tiemo102 = []
@@ -122,14 +107,7 @@ function nothing2() {
         max102 +=1
         let room2flo1 = document.getElementById('eccono102').value
         full102.push(room2flo1)
-        over102.push(room2flo1)
-        let enter102 = room2flo1 + Date()
-        teimo101.push(enter102)
-        localStorage.setItem('checkin102',teimo102)
         document.getElementById('occupant102').innerHTML = "occupant(s): " + full102
-        // save array it is ok to overwrite the key
-        let local102 = localStorage.setItem('room102',over102)
-        allcheckins.push(local102)
         document.getElementById('eccono102').value = ""
         document.getElementById('overflow102').innerHTML = ""
         document.getElementById('wrong102').innerHTML = ""
@@ -145,9 +123,6 @@ function beats2() {
     if (compare102 == full102[0] || compare102 == full102[1] || compare102 == full102[2] || compare102 == full102[3]) {
         if (compare102 == full102[0]) {
             max102 -=1
-            let exit102 = full102[0] + Date()
-            tiemo102.push(exit102)
-            localStorage.setItem('checkout102',tiemo102)
             // Remove Guest Part
             kicked102 = full102.splice(0,1)
             document.getElementById('occupant102').innerHTML = "occupant(s): " + full102
@@ -156,9 +131,6 @@ function beats2() {
         }
         else if (compare102 == full102[1]) {
             max102 -=1
-            let exit102 = full102[1] + Date()
-            tiemo102.push(exit102)
-            localStorage.setItem('checkout102',tiemo102)
             // Remove Guest Part
             kicked102 = full102.splice(1,1)
             document.getElementById('occupant102').innerHTML = "occupant(s): " + full102
@@ -167,9 +139,6 @@ function beats2() {
         }
         else if (compare102 == full102[2]) {
             max102 -=1
-            let exit102 = full102[2] + Date()
-            tiemo102.push(exit102)
-            localStorage.setItem('checkout102',tiemo102)
             // Remove Guest Part
             kicked102 = full102.splice(2,1)
             document.getElementById('occupant102').innerHTML = "occupant(s): " + full102
@@ -178,9 +147,6 @@ function beats2() {
         }
         else {
             max102 -=1
-            let exit102 = full102[3] + Date()
-            tiemo102.push(exit102)
-            localStorage.setItem('checkout102',tiemo102)
             // Remove Guest Part
             kicked102 = full102.splice(3,1)
             document.getElementById('occupant102').innerHTML = "occupant(s): " + full102
@@ -227,7 +193,6 @@ let access103 = "unlocked"
 let dissable103 = false
 let max103 = 0
 let full103 = []
-let over103 = []
 let kicked103 = []
 let teimo103 = []
 let tiemo103 = []
@@ -237,14 +202,7 @@ function nothing3() {
         max103 +=1
         let room3flo1 = document.getElementById('eccono103').value
         full103.push(room3flo1)
-        over103.push(room3flo1)
-        let enter103 = room3flo1 + Date()
-        teimo103.push(enter103)
-        localStorage.setItem('checkin103',teimo103)
         document.getElementById('occupant103').innerHTML = "occupant(s): " + full103
-        // save array it is ok to overwrite the key
-        let local103 = localStorage.setItem('room103',over103)
-        allcheckins.push(local103)
         document.getElementById('eccono103').value = ""
         document.getElementById('overflow103').innerHTML = ""
         document.getElementById('wrong103').innerHTML = ""
@@ -260,9 +218,6 @@ function beats3() {
     if (compare103 == full103[0] || compare103 == full103[1] || compare103 == full103[2] || compare103 == full103[3]) {
         if (compare103 == full103[0]) {
             max103 -=1
-            let exit103 = full103[0] + Date()
-            tiemo103.push(exit103)
-            localStorage.setItem('checkout103',tiemo103)
             // Remove Guest Part
             kicked103 = full103.splice(0,1)
             document.getElementById('occupant103').innerHTML = "occupant(s): " + full103
@@ -271,9 +226,6 @@ function beats3() {
         }
         else if (compare103 == full103[1]) {
             max103 -=1
-            let exit103 = full103[1] + Date()
-            tiemo103.push(exit103)
-            localStorage.setItem('checkout103',tiemo103)
             // Remove Guest Part
             kicked103 = full103.splice(1,1)
             document.getElementById('occupant103').innerHTML = "occupant(s): " + full103
@@ -282,9 +234,6 @@ function beats3() {
         }
         else if (compare103 == full103[2]) {
             max103 -=1
-            let exit103 = full103[2] + Date()
-            tiemo103.push(exit103)
-            localStorage.setItem('checkout103',tiemo103)
             // Remove Guest Part
             kicked103 = full103.splice(2,1)
             document.getElementById('occupant103').innerHTML = "occupant(s): " + full103
@@ -293,9 +242,6 @@ function beats3() {
         }
         else {
             max103 -=1
-            let exit103 = full103[3] + Date()
-            tiemo103.push(exit103)
-            localStorage.setItem('checkout103',tiemo103)
             // Remove Guest Part
             kicked103 = full103.splice(3,1)
             document.getElementById('occupant103').innerHTML = "occupant(s): " + full103
@@ -342,7 +288,6 @@ let access104 = "unlocked"
 let dissable104 = false
 let max104 = 0
 let full104 = []
-let over104 = []
 let kicked104 = []
 let teimo104 = []
 let tiemo104 = []
@@ -352,14 +297,7 @@ function nothing4() {
         max104 +=1
         let room4flo1 = document.getElementById('eccono104').value
         full104.push(room4flo1)
-        over104.push(room4flo1)
-        let enter104 = room4flo1 + Date()
-        teimo104.push(enter104)
-        localStorage.setItem('checkin104',teimo104)
         document.getElementById('occupant104').innerHTML = "occupant(s): " + full104
-        // save array it is ok to overwrite the key
-        let local104 = localStorage.setItem('room104',over104)
-        allcheckins.push(local104)
         document.getElementById('eccono104').value = ""
         document.getElementById('overflow104').innerHTML = ""
         document.getElementById('wrong104').innerHTML = ""
@@ -375,9 +313,6 @@ function beats4() {
     if (compare104 == full104[0] || compare104 == full104[1] || compare104 == full104[2] || compare104 == full104[3]) {
         if (compare104 == full104[0]) {
             max104 -=1
-            let exit104 = full104[0] + Date()
-            tiemo104.push(exit104)
-            localStorage.setItem('checkout104',tiemo104)
             // Remove Guest Part
             kicked104 = full104.splice(0,1)
             document.getElementById('occupant104').innerHTML = "occupant(s): " + full104
@@ -386,9 +321,6 @@ function beats4() {
         }
         else if (compare104 == full104[1]) {
             max104 -=1
-            let exit104 = full104[1] + Date()
-            tiemo104.push(exit104)
-            localStorage.setItem('checkout104',tiemo104)
             // Remove Guest Part
             kicked104 = full104.splice(1,1)
             document.getElementById('occupant104').innerHTML = "occupant(s): " + full104
@@ -397,9 +329,6 @@ function beats4() {
         }
         else if (compare104 == full104[2]) {
             max104 -=1
-            let exit104 = full104[2] + Date()
-            tiemo104.push(exit104)
-            localStorage.setItem('checkout104',tiemo104)
             // Remove Guest Part
             kicked104 = full104.splice(2,1)
             document.getElementById('occupant104').innerHTML = "occupant(s): " + full104
@@ -408,9 +337,6 @@ function beats4() {
         }
         else {
             max104 -=1
-            let exit104 = full104[3] + Date()
-            tiemo104.push(exit104)
-            localStorage.setItem('checkout104',tiemo104)
             // Remove Guest Part
             kicked104 = full104.splice(3,1)
             document.getElementById('occupant104').innerHTML = "occupant(s): " + full104
@@ -457,7 +383,6 @@ let access105 = "unlocked"
 let dissable105 = false
 let max105 = 0
 let full105 = []
-let over105 = []
 let kicked105 = []
 let teimo105 = []
 let tiemo105 = []
@@ -467,14 +392,7 @@ function nothing5() {
         max105 +=1
         let room5flo1 = document.getElementById('eccono105').value
         full105.push(room5flo1)
-        over105.push(room5flo1)
-        let enter105 = room5flo1 + Date()
-        teimo105.push(enter105)
-        localStorage.setItem('checkin105',teimo105)
         document.getElementById('occupant105').innerHTML = "occupant(s): " + full105
-        // save array it is ok to overwrite the key
-        let local105 = localStorage.setItem('room105',over105)
-        allcheckins.push(local105)
         document.getElementById('eccono105').value = ""
         document.getElementById('overflow105').innerHTML = ""
         document.getElementById('wrong105').innerHTML = ""
@@ -490,9 +408,6 @@ function beats5() {
     if (compare105 == full105[0] || compare105 == full105[1] || compare105 == full105[2] || compare105 == full105[3]) {
         if (compare105 == full105[0]) {
             max105 -=1
-            let exit105 = full105[0] + Date()
-            tiemo105.push(exit105)
-            localStorage.setItem('checkout105',tiemo105)
             // Remove Guest Part
             kicked105 = full105.splice(0,1)
             document.getElementById('occupant105').innerHTML = "occupant(s): " + full105
@@ -501,9 +416,6 @@ function beats5() {
         }
         else if (compare105 == full105[1]) {
             max105 -=1
-            let exit105 = full105[1] + Date()
-            tiemo105.push(exit105)
-            localStorage.setItem('checkout105',tiemo105)
             // Remove Guest Part
             kicked105 = full105.splice(1,1)
             document.getElementById('occupant105').innerHTML = "occupant(s): " + full105
@@ -512,9 +424,6 @@ function beats5() {
         }
         else if (compare105 == full105[2]) {
             max105 -=1
-            let exit105 = full105[2] + Date()
-            tiemo105.push(exit105)
-            localStorage.setItem('checkout105',tiemo105)
             // Remove Guest Part
             kicked105 = full105.splice(2,1)
             document.getElementById('occupant105').innerHTML = "occupant(s): " + full105
@@ -523,9 +432,6 @@ function beats5() {
         }
         else {
             max105 -=1
-            let exit105 = full105[3] + Date()
-            tiemo105.push(exit105)
-            localStorage.setItem('checkout105',tiemo105)
             // Remove Guest Part
             kicked105 = full105.splice(3,1)
             document.getElementById('occupant105').innerHTML = "occupant(s): " + full105
@@ -572,7 +478,6 @@ let access106 = "unlocked"
 let dissable106 = false
 let max106 = 0
 let full106 = []
-let over106 = []
 let kicked106 = []
 let teimo106 = []
 let tiemo106 = []
@@ -582,14 +487,7 @@ function nothing6() {
         max106 +=1
         let room6flo1 = document.getElementById('eccono106').value
         full106.push(room6flo1)
-        over106.push(room6flo1)
-        let enter106 = room6flo1 + Date()
-        teimo106.push(enter106)
-        localStorage.setItem('checkin106',teimo106)
         document.getElementById('occupant106').innerHTML = "occupant(s): " + full106
-        // save array it is ok to overwrite the key
-        let local106 = localStorage.setItem('room106',over106)
-        allcheckins.push(local106)
         document.getElementById('eccono106').value = ""
         document.getElementById('overflow106').innerHTML = ""
         document.getElementById('wrong106').innerHTML = ""
@@ -605,9 +503,6 @@ function beats6() {
     if (compare106 == full106[0] || compare106 == full106[1] || compare106 == full106[2] || compare106 == full106[3]) {
         if (compare106 == full106[0]) {
             max106 -=1
-            let exit106 = full106[0] + Date()
-            tiemo106.push(exit106)
-            localStorage.setItem('checkout106',tiemo106)
             // Remove Guest Part
             kicked106 = full106.splice(0,1)
             document.getElementById('occupant106').innerHTML = "occupant(s): " + full106
@@ -616,9 +511,6 @@ function beats6() {
         }
         else if (compare106 == full106[1]) {
             max106 -=1
-            let exit106 = full106[1] + Date()
-            tiemo106.push(exit106)
-            localStorage.setItem('checkout106',tiemo106)
             // Remove Guest Part
             kicked106 = full106.splice(1,1)
             document.getElementById('occupant106').innerHTML = "occupant(s): " + full106
@@ -627,9 +519,6 @@ function beats6() {
         }
         else if (compare106 == full106[2]) {
             max106 -=1
-            let exit106 = full106[2] + Date()
-            tiemo106.push(exit106)
-            localStorage.setItem('checkout106',tiemo106)
             // Remove Guest Part
             kicked106 = full106.splice(2,1)
             document.getElementById('occupant106').innerHTML = "occupant(s): " + full106
@@ -638,9 +527,6 @@ function beats6() {
         }
         else {
             max106 -=1
-            let exit106 = full106[3] + Date()
-            tiemo106.push(exit106)
-            localStorage.setItem('checkout106',tiemo106)
             // Remove Guest Part
             kicked106 = full106.splice(3,1)
             document.getElementById('occupant106').innerHTML = "occupant(s): " + full106
